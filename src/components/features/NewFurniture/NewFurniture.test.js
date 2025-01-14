fab0c5da3cf30e190dfb7027327ecc28362dedae
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2025-01-14 15:23:50 +0000

    Set current viewport mode (desktop,tablet, or mobile) in application state

--- src/components/features/NewFurniture/NewFurniture.test.js
+++ src/components/features/NewFurniture/NewFurniture.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { shallow } from 'enzyme';
 import NewFurniture from './NewFurniture';
 
+const mockStore = {
+  getState: () => ({
+    viewport: 'desktop',
+    categories: [],
+    products: [],
+  }),
+  subscribe: jest.fn(),
+  dispatch: jest.fn(),
+};
+
 describe('Component NewFurniture', () => {
   it('should render without crashing', () => {
-    const component = shallow(<NewFurniture />);
+    const component = shallow(
+      <Provider store={mockStore}>
+        <NewFurniture />
+      </Provider>
+    );
     expect(component).toBeTruthy();
   });
 });
--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setViewportMode } from '../../../redux/viewportRedux';
 import PropTypes from 'prop-types';
 
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
-const MainLayout = ({ children }) => (
-  <div>
-    <Header />
-    {children}
-    <Footer />
-  </div>
-);
+const MainLayout = ({ children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const updateViewportMode = () => {
+      const width = window.innerWidth;
+      if (width >= 1024) {
+        dispatch(setViewportMode('desktop'));
+      } else if (width >= 768 && width < 1024) {
+        dispatch(setViewportMode('tablet'));
+      } else if (width < 768 && width > 576) {
+        dispatch(setViewportMode('mediumMobile'));
+      } else dispatch(setViewportMode('mobile'));
+    };
+
+    updateViewportMode();
+    window.addEventListener('resize', updateViewportMode);
+
+    return () => {
+      window.removeEventListener('resize', updateViewportMode);
+    };
+  }, [dispatch]);
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
 
 MainLayout.propTypes = {
   children: PropTypes.node,
